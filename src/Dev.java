40bc49c964f66176f48aea1f71fc44dbe0525e70
levi <dantaslevi01@gmail.com>
2024-02-07 16:30:31 -0300

    mais classes

---
+++ src/Dev.java
@@ -0,0 +1,79 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcuidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+    }
+
+    public void progredir() {
+    }
+
+    public void calcularTotalXp() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcuidos() {
+        return conteudosConcuidos;
+    }
+
+    public void setConteudosConcuidos(Set<Conteudo> conteudosConcuidos) {
+        this.conteudosConcuidos = conteudosConcuidos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((conteudosInscritos == null) ? 0 : conteudosInscritos.hashCode());
+        result = prime * result + ((conteudosConcuidos == null) ? 0 : conteudosConcuidos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dev other = (Dev) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (conteudosInscritos == null) {
+            if (other.conteudosInscritos != null)
+                return false;
+        } else if (!conteudosInscritos.equals(other.conteudosInscritos))
+            return false;
+        if (conteudosConcuidos == null) {
+            if (other.conteudosConcuidos != null)
+                return false;
+        } else if (!conteudosConcuidos.equals(other.conteudosConcuidos))
+            return false;
+        return true;
+    }
+
+}
